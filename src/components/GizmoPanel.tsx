137d461f4e78166126697c1107d769673a6649ba
Jaws <nikordn@gmail.com>
2023-11-13 21:48:48 +0200

    refactor: :recycle: change how gizmo shows features

--- src/components/GizmoPanel.tsx
+++ src/components/GizmoPanel.tsx
@@ -1,45 +1,95 @@
-import Link from 'next/link'
-import { useState } from 'react'
-import { AiOutlineFileSearch } from 'react-icons/ai'
-import { FaRegImage } from 'react-icons/fa'
-import { TbBoxMultiple } from 'react-icons/tb'
+import { useState, useCallback } from 'react'
+import { IoMdCheckboxOutline } from 'react-icons/io'
 import Modal from './Modal'
+import { useSettingsStore } from '@/zustand/settings'
+
+const featureTypes = {
+  chat: 'chat',
+  imageGeneration: 'image generation',
+  documentQuery: 'document query',
+}
+
+type CheckboxProps = {
+  label: string
+  feature: string
+  selectedFeature: string | null
+  onSelectFeature: (feature: string) => void
+}
+
+const Checkbox: React.FC<CheckboxProps> = ({
+  label,
+  feature,
+  selectedFeature,
+  onSelectFeature,
+}) => {
+  return (
+    <div className="flex justify-center items-center">
+      <input
+        id={`checkbox-${feature}`}
+        type="checkbox"
+        className="w-4 h-4 bg-gray-100 border-gray-300 rounded"
+        checked={selectedFeature === feature}
+        onChange={() => onSelectFeature(feature)}
+      />
+      <label htmlFor={`checkbox-${feature}`} className="ms-2 text-sm font-medium text-gray-200">
+        {label}
+      </label>
+    </div>
+  )
+}
 
 export default function GizmoPanel() {
   const [modalOpen, setModalOpen] = useState(false)
 
-  const handleOpenModal = () => setModalOpen(true)
-  const handleCloseModal = () => setModalOpen(false)
+  const useChat = useSettingsStore((state) => state.useChat)
+  const updateUseChat = useSettingsStore((state) => state.updateUseChat)
+
+  const useImageGeneration = useSettingsStore((state) => state.useImageGeneration)
+  const updateUseImageGeneration = useSettingsStore((state) => state.updateUseImageGeneration)
+
+  const useDocumentQuery = useSettingsStore((state) => state.useDocumentQuery)
+  const updateUseDocumentQuery = useSettingsStore((state) => state.updateUseDocumentQuery)
+
+  const getDefaultFeature = () => {
+    if (useChat) return featureTypes.chat
+    if (useImageGeneration) return featureTypes.imageGeneration
+    if (useDocumentQuery) return featureTypes.documentQuery
+    return null
+  }
+
+  const [selectedFeature, setSelectedFeature] = useState<string | null>(getDefaultFeature())
+
+  const handleSelectFeature = useCallback(
+    (feature: string) => {
+      setSelectedFeature(feature)
+      updateUseChat(feature === featureTypes.chat)
+      updateUseImageGeneration(feature === featureTypes.imageGeneration)
+      updateUseDocumentQuery(feature === featureTypes.documentQuery)
+    },
+    [updateUseChat, updateUseImageGeneration, updateUseDocumentQuery],
+  )
 
   return (
     <>
       <button
-        aria-label="Try other features"
+        aria-label="Features"
         className="flex gap-2 items-center p-4 rounded font-medium text-xl bg-gray-800 active:text-green-200"
-        onClick={handleOpenModal}
+        onClick={() => setModalOpen(true)}
       >
-        <TbBoxMultiple />
-        <p className="text-sm">Try other features</p>
+        <IoMdCheckboxOutline />
+        <p className="text-sm">Features</p>
       </button>
-      <Modal isOpen={modalOpen} onClose={handleCloseModal}>
-        <div className="flex flex-col gap-2">
-          <Link href="/dalle">
-            <button
-              aria-label="Dalle 3"
-              className="flex w-full gap-2 items-center p-4 rounded font-medium text-xl bg-gray-800 active:text-green-200"
-            >
-              <FaRegImage />
-              <p className="text-sm">Dalle 3</p>
-            </button>
-          </Link>
-          <button
-            aria-label="Document search"
-            className="flex gap-2 items-center p-4 rounded font-medium text-xl bg-gray-800 active:text-green-200"
-            onClick={() => alert('Not implemented yet!')}
-          >
-            <AiOutlineFileSearch />
-            <p className="text-sm">Document search</p>
-          </button>
+      <Modal isOpen={modalOpen} onClose={() => setModalOpen(false)}>
+        <div className="flex flex-col items-start gap-2">
+          {Object.entries(featureTypes).map(([key, feature]) => (
+            <Checkbox
+              key={key}
+              label={`Use ${feature}`}
+              feature={feature}
+              selectedFeature={selectedFeature}
+              onSelectFeature={handleSelectFeature}
+            />
+          ))}
         </div>
       </Modal>
     </>
